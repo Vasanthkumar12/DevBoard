e6ce3b39f955c8bce0c767db4e553180c87d3ecb
Vasanthkumar M <vasanth23402@gmail.com>
2025-05-12 06:18:54 +0530

    Auth Context done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { PrivateRoute } from './components/PrivateRoute'
+import { Dashboard } from './components/pages/Dashboard'
+import { Login } from './components/pages/Login'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/dashboard' element={<PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>} />  
+      </Routes>      
     </>
   )
 }
--- src/components/contexts/AuthContext.jsx
+++ src/components/contexts/AuthContext.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// 1st step is create a Auth Context
 
-export const AuthContext = () => {
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  
+  const [user, setUser] = useState('')
+
+  const login = (userData) => {
+    setUser(userData)
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
+
   return (
-    <div>
-        
-    </div>
+    <AuthContext.Provider value={{user, login, logout}}>
+      {children}
+    </AuthContext.Provider>
   )
 }
+
+// 2nd step is go and wrap your App component by using <AuthProvider>
